ed035022753e7536f622cf319dbf4889e868a123
Mehran Sadeghi <mehran.sadeghi@karnameh.ir>
2025-01-07 12:34:56 +0330

    initial commit

---
+++ src/components/ui/icons/styles.ts
@@ -0,0 +1,7 @@
+import { tv } from "tailwind-variants";
+
+const iconStyles = tv({
+    base: 'group/iconContainedPrimary:fill-primary group/iconContainedSecondary:fill-secondary'
+});
+
+export default iconStyles;
---
+++ src/components/ui/textField/index.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import { tv } from "tailwind-variants";
+
+const styles = tv({
+    slots: {
+        root: 'w-full h-10 ',
+    }
+})
+
+const TextField: FC = () => {
+
+    return (
+        <div></div>
+    )
+}
+
+export default TextField;
